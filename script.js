8e2ddb0ddae5cc1dc370b4cef1019a216b4ad77c
Pedro Peroni <pedrohperoni@gmail.com>
2021-11-11 16:39:07 -0300

    user login

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-axios.get("https://mock-api.driven.com.br/api/v4/uol/messages").then(renderChat)
+// user log in request
+
+const user = prompt("Qual seu nome?");
+const userPromise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants",{name: user})
+userPromise.then(teste)
+userPromise.catch(error)
+
+function teste(resposta){
+   console.log(resposta);
+}
+
+function error(){
+   user = prompt("Esse nome é inválido ou já está em uso, digite outro nome!")
+}
+
+// user status check (check every 5 seconds)
+
+const userUpdatePromise = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", {name:user})
+userUpdatePromise.catch()
+
+function connectionLost(){
+   location.reload()
+}
+
+// load messages from server (load every 3 seconds)
+
+setInterval(axios.get("https://mock-api.driven.com.br/api/v4/uol/messages").then(renderChat), 3000)
 
 
 function renderChat(chatInfo){
@@ -7,25 +33,24 @@ function renderChat(chatInfo){
    console.log(info)
 
    for(i=0;i<=info.length;i++){
-      let defaultText = "";
-      let receiver = "";
+      let defaultText = '';
+      let receiver = '';
 
       if(info[i].type == "message"){
-            defaultText = "para"
-            receiver = info[i].to
+            defaultText = "<p>para</p>"
+            receiver = `<strong>${info[i].to}</strong>`
       } else if (info[i].type == "private_message"){
-            defaultText = "reservadamente para"
-            receiver = info[i].to
+            defaultText = "<p>reservadamente para</p>"
+            receiver = `<strong>${info[i].to}</strong>`
       }
-
 
    messageList.innerHTML += 
    `         
    <div class="messageBox ${info[i].type}">
-      <span>${info[i].time}</span>
+      <span>(${info[i].time})</span>
       <strong>${info[i].from}</strong>
-      <p>${defaultText}</p>
-      <strong>${receiver}</strong>
+      ${defaultText}
+      ${receiver}
       <p>${info[i].text}</p>
    </div>
    `
@@ -33,3 +58,24 @@ function renderChat(chatInfo){
    messageList.scrollIntoView()
    }
 }
+
+
+// send messages to the server
+let inputMessage = document.querySelector(".sendMessage").value;
+
+
+
+messagePromise.then(sendMessage)
+
+function sendMessage(){
+   inputMessage = document.querySelector(".sendMessage").value;
+   console.log(inputMessage, user,"aseae")
+
+}
+
+const messagePromise = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", {
+   from: user,
+   to: any,
+   text: inputMessage,
+   type: message,
+})
