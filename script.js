9c166498f7f3c3cf6d77712e8946785dada60947
Pedro Peroni <pedrohperoni@gmail.com>
2021-11-11 15:41:37 -0300

    chat messages

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-getChatMessages()
+axios.get("https://mock-api.driven.com.br/api/v4/uol/messages").then(renderChat)
 
-function getChatMessages(){
-   axios.get("https://mock-api.driven.com.br/api/v4/uol/messages").then(renderChat)
-}
 
 function renderChat(chatInfo){
-   const x = chatInfo.data
+   const info = chatInfo.data
    const messageList = document.querySelector("main")
-   console.log(x)
+   console.log(info)
 
-   for(i=0;i<=x.length;i++){
+   for(i=0;i<=info.length;i++){
+      let defaultText = "";
+      let receiver = "";
+
+      if(info[i].type == "message"){
+            defaultText = "para"
+            receiver = info[i].to
+      } else if (info[i].type == "private_message"){
+            defaultText = "reservadamente para"
+            receiver = info[i].to
+      }
+
+
    messageList.innerHTML += 
-   ` <div class="message status">
-      <p><span>(${x[i].time})</span><strong>${x[i].from}</strong>entra na sala...</p>
-   </div>`
+   `         
+   <div class="messageBox ${info[i].type}">
+      <span>${info[i].time}</span>
+      <strong>${info[i].from}</strong>
+      <p>${defaultText}</p>
+      <strong>${receiver}</strong>
+      <p>${info[i].text}</p>
+   </div>
+   `
+
+   messageList.scrollIntoView()
    }
 }
