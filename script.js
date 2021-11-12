4523a70cc7cd9a9d1313910b5d23ecc746797809
Pedro Peroni <pedrohperoni@gmail.com>
2021-11-12 14:54:07 -0300

    send message

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // user log in request
 
-const user = prompt("Qual seu nome?");
+let user = prompt("Qual seu nome?");
 const userPromise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants",{name: user})
 userPromise.then(teste)
 userPromise.catch(error)
 
 function teste(resposta){
-   console.log(resposta);
+   console.log(resposta,"success");
 }
 
 function error(){
@@ -20,62 +20,66 @@ userUpdatePromise.catch()
 
 function connectionLost(){
    location.reload()
+   console.log("userUpdated")
 }
 
 // load messages from server (load every 3 seconds)
 
-setInterval(axios.get("https://mock-api.driven.com.br/api/v4/uol/messages").then(renderChat), 3000)
+  const messagesPromise =  axios.get("https://mock-api.driven.com.br/api/v4/uol/messages")
+  messagesPromise.then(displayMessages)
+   console.log("try")
 
+   function displayMessages(chatInfo){
+      const info = chatInfo.data
+      const messageList = document.querySelector("main")
 
-function renderChat(chatInfo){
-   const info = chatInfo.data
-   const messageList = document.querySelector("main")
-   console.log(info)
+      console.log(info, "console log")
+   
+      for(i=0;i<=info.length;i++){
+         let defaultText = '';
+         let receiver = '';
+   
+         if(info[i].type === "message"){
+               defaultText = "<p>para</p>"
+               receiver = `<strong>${info[i].to}</strong>`
+         } else if (info[i].type === "private_message"){
+               defaultText = "<p>reservadamente para</p>"
+               receiver = `<strong>${info[i].to}</strong>`
+         }
 
-   for(i=0;i<=info.length;i++){
-      let defaultText = '';
-      let receiver = '';
-
-      if(info[i].type == "message"){
-            defaultText = "<p>para</p>"
-            receiver = `<strong>${info[i].to}</strong>`
-      } else if (info[i].type == "private_message"){
-            defaultText = "<p>reservadamente para</p>"
-            receiver = `<strong>${info[i].to}</strong>`
+      messageList.innerHTML += 
+      `         
+      <div class="messageBox ${info[i].type}">
+         <span>(${info[i].time})</span>
+         <strong>${info[i].from}</strong>
+         ${defaultText}
+         ${receiver}
+         <p>${info[i].text}</p>
+      </div>
+      `
+      messageList.scrollIntoView(false)
       }
-
-   messageList.innerHTML += 
-   `         
-   <div class="messageBox ${info[i].type}">
-      <span>(${info[i].time})</span>
-      <strong>${info[i].from}</strong>
-      ${defaultText}
-      ${receiver}
-      <p>${info[i].text}</p>
-   </div>
-   `
-
-   messageList.scrollIntoView()
    }
-}
 
 
 // send messages to the server
-let inputMessage = document.querySelector(".sendMessage").value;
-
-
-
-messagePromise.then(sendMessage)
 
 function sendMessage(){
-   inputMessage = document.querySelector(".sendMessage").value;
-   console.log(inputMessage, user,"aseae")
 
-}
+let message = document.querySelector(".inputMessage").value
+console.log(message)
 
-const messagePromise = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", {
+axios.post("https://mock-api.driven.com.br/api/v4/uol/messages",{
    from: user,
-   to: any,
-   text: inputMessage,
-   type: message,
+	to: "Todos",
+	text: message,
+	type: "message"
 })
+.then(function(response){
+   console.log("mensagem enviada",response)
+   messagesPromise.then(displayMessages)
+})
+.catch(function(error){
+   console.log("erro de envio",error.response)
+});
+}
